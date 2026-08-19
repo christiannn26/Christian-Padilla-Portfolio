6de2b93f2abcfaa456975ffb7492ad5873c9412e
ChristianPadilla <padilla.christian2003@gmail.com>
2026-08-20 00:32:13 +0800

    Configure GitHub Actions deployment

--- portfolio-app/vite.config.ts
+++ portfolio-app/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Christian-Padilla-Portfolio/',
   plugins: [
     react(),
     tailwindcss(),
